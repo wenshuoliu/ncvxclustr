b2719ed4aba94875ae04aee65933ce2f2d5bace9
wenshuoliu <wenshuoliu@gmail.com>
2017-04-12 11:49:24 -0500

    Set the package structure.

---
+++ R/ncvxclustr.R
@@ -0,0 +1,11 @@
+#'ncvxclustr: non-convex clustering
+#'
+#'algorithm to solve the clustering problem by non-convex penalization
+#'
+#'@section Functions:
+#'
+#'@docType package
+#'@name ncvxclustr
+#'@useDynLib ncvxclustr
+#'@importFrom Rcpp sourceCpp
+NULL
---
+++ src/ncvxclustr_types.h
@@ -0,0 +1,6 @@
+#include<RcppEigen.h>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::MatrixXi;
+using Eigen::VectorXi;
